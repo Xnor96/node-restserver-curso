3a13e50fee5dc8fed89015bc6c4d40ff55f35268
Xnor96 <mariopachecoherrada@gmail.com>
2020-08-10 20:39:20 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,25 @@ puerto
 ==========
 */
 process.env.PORT = process.env.PORT || 3000;
+
+/*
+==========
+puerto
+==========
+*/
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+/*
+==========
+puerto
+==========
+*/
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://xnor:thefuture@cluster0.e6wja.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlDB;
